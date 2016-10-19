71489677c0aa01f963dc919a0d0852edff0f69bd
오준영 <icons@hogi.co.kr>
2016-10-19 17:20:34 +0900

    1

--- www/views/index.php
+++ www/views/index.php
@@ -60,6 +60,12 @@
 					</td>
 				</tr>
 			</table>
+			<div class="footer">
+				<div class="container">
+					<p class="contact">03127 서울시 종로구 율곡로 190, 여전도회관 411호<br>
+						T. 02)2623-3100 &nbsp;&nbsp;&nbsp;&nbsp; F. 02)2038-3112</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	<a href="#next-scene" class="next active"><span class="sr-only">다음</span></a>
