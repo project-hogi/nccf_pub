5233be9b1f5395fc0e215b55d55b2c989f0ee93b
오준영 <icons@hogi.co.kr>
2016-10-19 14:49:11 +0900

    1

--- www/resources-dev/front/jspack/3.part/layout-index.js
+++ www/resources-dev/front/jspack/3.part/layout-index.js
@@ -4,7 +4,136 @@
 
 	$(document).on("appinit", function() {
 		$("body.layout-index").each(function() {
-			$(".lnb").eachReadyScope(function() {
+			$(".scene-content-container img[usemap]").eachReadyScope(function() {
+				$(this).rwdImageMaps();
+			});
+			$(".scene-container-wrap").eachReadyScope(function() {
+				var $obj = $(this);
+				var $control = $(".lnb");
+				var $controlAs = $control.find(">ul>li>a");
+				var $next = $obj.find(".next");
+				var $sceneBgContainer = $obj.find(".scene-bg-container");
+				var $sceneBgEq0 = $sceneBgContainer.find(".scene-0");
+				var $sceneContentContainer = $obj.find(".scene-content-container");
+				var $sceneContentEq0 = $sceneContentContainer.find(".scene-0");
+				var $siteLogo = $(".site-logo");
+				var activeSceneIndex = 0;
+				var lastTriggingMousewheelDatetime = null;
+				var lastTriggingMousewheelPosition = null;
+				var sceneSize = $controlAs.size();
+				var sceneHeight = $obj.height();
+
+				$(window).on("resized", function() {
+					sceneHeight = $obj.height();
+					runAnimation();
+				});
+
+				$siteLogo.on("click", function(event) {
+					if (!$obj.filter(":visible").size())
+						return;
+
+					event.preventDefault();
+
+					run(0);
+				});
+
+				$control.on("mouseover", ">ul>li>a", function(event) {
+					if (!$obj.filter(":visible").size())
+						return;
+
+					event.preventDefault();
+
+					var index = $controlAs.index(this);
+
+					run(index);
+				});
+
+				$next.on("click", function(event) {
+					event.preventDefault();
+
+					if (activeSceneIndex < sceneSize - 1)
+						run(activeSceneIndex + 1);
+				});
+
+				$(window).on("mousewheel DOMMouseScroll", function(event) {
+					if (!$obj.filter(":visible").size())
+						return;
+
+					var position = null;
+
+					if (event && event.originalEvent && event.originalEvent.deltaY)
+						position = event.originalEvent.deltaY >= 0 ? 1 : -1;
+					else if (event && event.originalEvent && event.originalEvent.wheelDelta)
+						position = event.originalEvent.wheelDelta <= 0 ? 1 : -1;
+					else
+						return;
+
+					if ((lastTriggingMousewheelPosition == null || lastTriggingMousewheelPosition != null && position == lastTriggingMousewheelPosition) && lastTriggingMousewheelDatetime != null && new Date().getTime() - lastTriggingMousewheelDatetime < 600)
+						return;
+
+					var index = activeSceneIndex;
+
+					if (position === 1) {
+						if (activeSceneIndex < sceneSize - 1)
+							index = activeSceneIndex + 1;
+					} else {
+						if (activeSceneIndex > 0)
+							index = activeSceneIndex - 1;
+					}
+
+					run(index);
+
+					lastTriggingMousewheelDatetime = new Date().getTime();
+					lastTriggingMousewheelPosition = position;
+				});
+
+				function run(index) {
+					if (!$obj.filter(":visible").size())
+						return;
+
+					if (index === null || activeSceneIndex === index)
+						return;
+
+					activeSceneIndex = index;
+
+					$controlAs.parent().removeClass("active");
+					$controlAs.eq(activeSceneIndex).parent().addClass("active");
+
+					if (activeSceneIndex >= sceneSize - 1)
+						$next.removeClass("active");
+					else
+						$next.addClass("active");
+
+					$sceneBgContainer.removeClass("active").eq(activeSceneIndex).addClass("active");
+					$sceneContentContainer.removeClass("active").eq(activeSceneIndex).addClass("active");
+
+					runAnimation();
+				}
+
+				function runAnimation() {
+					if (!$obj.filter(":visible").size())
+						return;
+
+					var targetPosition = activeSceneIndex * sceneHeight * -1;
+					var duration = 400;
+					var top = parseInt($sceneBgEq0.css("margin-top"));
+
+					duration += Math.round(Math.abs(targetPosition - top) / sceneHeight * 300);
+
+					$sceneBgEq0.stop().animate({
+						"margin-top" : targetPosition
+					}, {
+						duration : duration,
+						easing : "easeInOutExpo"
+					});
+
+					$sceneContentEq0.stop().animate({
+						"margin-top" : targetPosition
+					}, {
+						duration : Math.round(duration * 1.2),
+						easing : "easeInOutExpo"
+					});
+				}
 			});
 		});
 	});
--- www/views/index.php
+++ www/views/index.php
@@ -5,12 +5,64 @@
 <?php require "part/app-header.php"; ?>
 <?php require "part/header.php"; ?>
 <?php require "part/header-main.php"; ?>
-<div class="visual">
-	<div class="eq-0">
-		<div class="container">
-			<a href="#">자세히보기</a>
+<div class="scene-container-wrap">
+	<div class="scene-container scene-bg-container">
+		<div class="scene-0 active"><div class="eq-0"></div><div class="eq-1"></div></div>
+		<div class="scene-1"><div class="eq-0"></div><div class="eq-1"></div></div>
+		<div class="scene-2"><div class="eq-0"></div><div class="eq-1"></div></div>
+		<div class="scene-3"><div class="eq-0"></div><div class="eq-1"></div></div>
+	</div>
+	<div class="scene-container scene-content-container">
+		<div class="scene-0 active">
+			<table>
+				<tr>
+					<td>
+						<img src="<?php echo $config["imageDir"] . "/part/layout-index/scene-content-eq-0.png"; ?>" alt="나에게 우리를 더하다. 축제소개, 자세히보기" usemap="#scene-content-0" />
+						<map name="scene-content-0">
+							<area shape="rect" coords="942,505,1182,565" href="/front/introduce">
+						</map>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="scene-1">
+			<table>
+				<tr>
+					<td>
+						<img src="<?php echo $config["imageDir"] . "/part/layout-index/scene-content-eq-1.png"; ?>" alt="프로그램, 자세히보기" usemap="#scene-content-1" />
+						<map name="scene-content-1">
+							<area shape="rect" coords="942,505,1182,565" href="/front/program">
+						</map>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="scene-2">
+			<table>
+				<tr>
+					<td>
+						<img src="<?php echo $config["imageDir"] . "/part/layout-index/scene-content-eq-2.png"; ?>" alt="북서울꿈의숲, 관람안내, 자세히보기" usemap="#scene-content-2" />
+						<map name="scene-content-2">
+							<area shape="rect" coords="942,505,1182,565" href="/front/program">
+						</map>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="scene-3">
+			<table>
+				<tr>
+					<td>
+						<img src="<?php echo $config["imageDir"] . "/part/layout-index/scene-content-eq-3.png"; ?>" alt="생활문화진흥원, 자세히보기" usemap="#scene-content-3" />
+						<map name="scene-content-3">
+							<area shape="rect" coords="942,505,1182,565" href="http://www.nccf.or.kr/" target="_blank">
+						</map>
+					</td>
+				</tr>
+			</table>
 		</div>
 	</div>
+	<a href="#next-scene" class="next active"><span class="sr-only">다음</span></a>
 </div>
 <?php require "part/footer.php"; ?>
 <?php require "part/app-footer.php"; ?>
