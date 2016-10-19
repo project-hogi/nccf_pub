e56cfd86e1d651582fb7011f6b8db8eec9ee927f
오준영 <icons@hogi.co.kr>
2016-10-19 13:28:02 +0900

    1

--- www/resources-dev/front/jspack/3.part/layout-index.js
+++ www/resources-dev/front/jspack/3.part/layout-index.js
@@ -5,11 +5,6 @@
 	$(document).on("appinit", function() {
 		$("body.layout-index").each(function() {
 			$(".lnb").eachReadyScope(function() {
-				$(this).on("click", "a", function(event) {
-					event.preventDefault();
-					
-					alert("준비 중 입니다.");
-				});
 			});
 		});
 	});
--- www/views/introduce.php
+++ www/views/introduce.php
@@ -1,1 +1,11 @@
-<meta http-equiv="refresh" content="1;url=introduce-greetings.php">
+<?php $pagePath = isset($_pagePath) ? $_pagePath : __FILE__; ?>
+<?php $bodyClass = "layout-page module-content"; ?>
+<?php require "part/sitemap.php"; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/lib.php"; ?>
+<?php require "part/app-header.php"; ?>
+<?php require "part/header.php"; ?>
+<?php require "part/header-sub.php"; ?>
+
+<?php require "part/footer-sub.php"; ?>
+<?php require "part/footer.php"; ?>
+<?php require "part/app-footer.php"; ?>
