584f2fa743c2fc488737dd0db5256d7d5a3fb993
오준영 <icons@hogi.co.kr>
2016-10-26 11:30:29 +0900

    1

--- www/views/part/header-sub.php
+++ www/views/part/header-sub.php
@@ -2,3 +2,5 @@
 <div id="content">
 	<div class="container">
 		<h2 class="page-title"><span><?php echo getPageName($pagePath); ?></span></h2>
+		<div class="eq-0"></div>
+		<div class="eq-1"></div>
